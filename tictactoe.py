c3e56dfce4d007b3c53c87852d8f6f7a278a363e
Victor Baraka <barakavictor397@gmail.com>
2024-09-15 23:10:10 +0300

    debugging1: messed around with the code trying to fix the overwriting bug Also should have branched but or stashed but what the heck yk

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 """ 
-TicTacToe -------------------------------------------------"""
+TicTacToe -------------------------------------------------
+"""
 
 import random
 
@@ -13,18 +14,22 @@ class TicTactoe:
             row = []
             for j in range (3):
                 row.append("-")
-                self.row.append(row)
+                self.board.append(row)
     
-    def get_random_player_first(self):
+    
+    def get_random_first_player(self):
         return random.randint(0,1)
     
 
     def fix_spot(self,row,col,player):
         self.board [row][col] = player
-        board_values =  set()
+       
+    def player_has_won (self,player):
+        n = len(self.board)
+    board_values = set()
 
         #check rows
-        for i in range(n):
+    for i in range(n):
             for j in range (n):
                 board_values.add(self.board[i][j])
                 if board_values == {player}:
@@ -33,7 +38,7 @@ class TicTactoe:
                     board_values.clear()
 
         #check cols
-        for i in range (n):
+    for i in range (n):
             for j in range (n):
                 board_values.add(self.board[j][i])
                 if  board_values == {player}:
@@ -42,7 +47,13 @@ class TicTactoe:
                     board_values.clear()
 
         #check diagonals
-        for i in range(n):
+    for i in range(n):
+            board_values.add(self.board[i][i])
+            if board_values == {player}
+                return True
+            else:
+                board_values.clear()
+
             board_values.add(self.board[0][2])
             board_values.add(self.board[1][1])
             board_values.add(self.board[2][0])
@@ -56,9 +67,58 @@ class TicTactoe:
             for item in row:
                 if item == '-':
                     return False
-                else:
-                    return True
+          
+          return True
     
-    def swap_player_turn
+    def swap_player_turn(self, player):
+        return 'X' if player == 'O' else 'O'
+    
+    def show_board(self):
+        for row in self.board:
+            for item in row:
+                print (item, end= ' ')
+                print()
 
-   
+    def start(self):
+        self.create_board()
+        player = 'X' if self.get_random_first_player()== 1 else 'O'
+        game_over = False
+
+        while not game_over:
+            try:
+                self.show_board()
+                print(f'\nPlayer {player} turn')
+
+                row,col = list(
+                    map(int, input('enter row and column numbers to fix spot: ').split() 
+
+                    )
+                ) 
+                print()
+
+                if col is None:
+                    raise ValueError(
+                        'not enough values to unpack(expected 2, got 1)'
+                    )
+
+                self.fix_spot(row -1 ,col -1, player)
+                game_over = self.has_player_won(player)
+                if game_over:
+                    print(f'Player {player} wins the game!')
+                    continue
+
+                game_over = self.is_board_filled()
+                if game_over:
+                    print('match draw!')
+                    continue
+
+                player = self.swap_player_turn(player)
+            except ValueError as err:
+                print (err)
+
+                print()
+                self.show_board()
+
+if __name__ == '__main__':
+    tic_tac_toe = TicTactoe()
+    tic_tac_toe.start()  
